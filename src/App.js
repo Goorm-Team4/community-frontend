436ab141629f3d68ecc4c7f472da37a0c179b9b4
ssuzn <tnwls_723@naver.com>
2024-11-01 15:51:52 +0900

    feat(#2): 회원가입 모달 창 UI 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import LoginModal from './components/Modal/LoginModal';
+import SignupModal from './components/Modal/SignupModal';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       {/* 레이아웃 확인하기 위함 */}
       <Route path="/login" element={<LoginModal />} />
+      <Route path="/sign-up" element={<SignupModal />} />
     </Routes>
 
   );
